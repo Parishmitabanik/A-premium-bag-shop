1b7aaf07de569fa8cd8455308545211696eabc28
Parishmita Banik <parishmitabanik2005@gmail.com>
2026-05-31 11:10:45 +0530

    ready for deployment

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -3,13 +3,18 @@ const config=require("config")
 const dbgr=require("debug")("development:mongoose")
 
 
-mongoose
-.connect(`${config.get("MONGODB_URI")}/Apremiumbagshop`)
-.then(function(){
-    dbgr("connected");
-})
-.catch(function(err){
-    dbgr(err)
-})
+// mongoose
+// .connect(`${config.get("MONGODB_URI")}/Apremiumbagshop`)
+// .then(function(){
+//     dbgr("connected");
+// })
+// .catch(function(err){
+//     dbgr(err)
+// })
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log("MongoDB error:", err));
+
+module.exports = mongoose.connection;
 
 module.exports=mongoose.connection;
